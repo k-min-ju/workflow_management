be4c44c8f0c14cb488ac396b953662701b04bdf3
mjkim <alswn8065@gmail.com>
2024-11-15 23:30:22 +0900

    Set logic to move to workflow page when going back after logging in

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,6 @@
 'use client';
-import React, { useEffect } from 'react';
-import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
+import React from 'react';
 import LoginForm from '@/app/components/login/LoginForm';
-import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
 import styles from '@/app/components/login/login-form.module.scss';
 
 /**
@@ -11,14 +8,6 @@ import styles from '@/app/components/login/login-form.module.scss';
  * @constructor
  */
 export default function Login(): React.JSX.Element {
-  const { data: session, status } = useSession();
-  const router: AppRouterInstance = useRouter();
-
-  useEffect((): void => {
-    if (session && status === 'authenticated') {
-      router.replace('/workflow');
-    }
-  }, [session, status]);
   return (
     <div className={styles.loginContainer}>
       <LoginForm />
