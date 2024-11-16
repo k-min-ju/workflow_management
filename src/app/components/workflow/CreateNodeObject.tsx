e24f6e86dd70ad313b7b7d3b0cbf543ae9bf159d
mjkim <alswn8065@gmail.com>
2024-11-16 22:21:36 +0900

    Implement component that dynamically creates flow nodes

--- src/app/components/workflow/CreateNodeObject.tsx
+++ src/app/components/workflow/CreateNodeObject.tsx
@@ -1,16 +1,27 @@
-import React from 'react';
-import { CreateNodeObjectProps } from '@/types/xyflow';
+import React, { DragEvent } from 'react';
+import { CreateNodeObjectProps, FLOW_OBJECT_COLOR_KEY } from '@/types/xyflow';
+import { DATA_TRANSFER_TYPE } from '@/app/configs/constants';
 
 /**
  * Workflow node object creation common components
  * @param className - The class name applied to the node object
  * @param muiIcon - mui icon component to represent a node object
  * @param labelText - title representing the node object
+ * @param nodeType
  * @constructor
  */
-export default function CreateNodeObject({ className, muiIcon, labelText }: CreateNodeObjectProps): React.JSX.Element {
+export default function CreateNodeObject({
+  className,
+  muiIcon,
+  labelText,
+  nodeType
+}: CreateNodeObjectProps): React.JSX.Element {
+  const onDragStart = (event: DragEvent, nodeType: FLOW_OBJECT_COLOR_KEY): void => {
+    event.dataTransfer.effectAllowed = 'move';
+    event.dataTransfer.setData(DATA_TRANSFER_TYPE, nodeType);
+  };
   return (
-    <div className={className} draggable="true">
+    <div className={className} draggable onDragStart={(event: DragEvent): void => onDragStart(event, nodeType)}>
       {muiIcon}
       <div className="label">{labelText}</div>
     </div>
--- src/app/components/workflow/XYFlow.tsx
+++ src/app/components/workflow/XYFlow.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef, useState } from 'react';
+import React, { DragEvent, useCallback, useRef, useState } from 'react';
 import {
   addEdge,
   applyEdgeChanges,
@@ -12,55 +12,43 @@ import {
   ReactFlow,
   ReactFlowProvider
 } from '@xyflow/react';
-import { Connection } from '@xyflow/system/dist/esm/types/general';
-import { UseRef, UseState } from '@/types/common';
+import { XYPosition } from '@xyflow/system';
 import { FLOW_OBJECT_COLOR_KEY, FLOW_OBJECT_COLOR_VALUE, FlowEdge, FlowNode } from '@/types/xyflow';
+import type { ReactFlowInstance } from '@xyflow/react/dist/esm/types';
+import { Connection } from '@xyflow/system/dist/esm/types/general';
+import Sidebar from '@/app/components/workflow/Sidebar';
+import { UseRef, UseState } from '@/types/common';
+import { DATA_TRANSFER_TYPE, FLOW_NODE_PREFIX, FLOW_OBJECT_COLORS } from '@/app/configs/constants';
 import '@xyflow/react/dist/style.css';
 import styles from '@/app/components/workflow/workflow.module.scss';
-import { FLOW_OBJECT_COLORS } from '@/app/configs/constants';
-import Sidebar from '@/app/components/workflow/Sidebar';
 
 export default function XYFlow(): React.JSX.Element {
-  const initialNodes: FlowNode[] = [
+  const getId = (): string => `${FLOW_NODE_PREFIX}-${new Date().getTime()}`;
+  const sampleNodes: FlowNode[] = [
     {
-      id: '1',
+      id: getId(),
       type: 'input',
-      data: { label: 'Input Node' },
-      position: { x: 250, y: 25 },
+      data: { label: 'input node' },
+      position: { x: 0, y: 0 },
       style: { backgroundColor: FLOW_OBJECT_COLORS.input }
-    },
-    {
-      id: '2',
-      data: { label: <div>Default Node</div> },
-      position: { x: 100, y: 125 },
-      style: { backgroundColor: FLOW_OBJECT_COLORS.other }
-    },
-    {
-      id: '3',
-      type: 'output',
-      data: { label: 'Output Node' },
-      position: { x: 250, y: 250 },
-      style: { backgroundColor: FLOW_OBJECT_COLORS.output }
     }
   ];
-  const initialEdges: FlowEdge[] = [
-    { id: 'e1-2', source: '1', target: '2' },
-    { id: 'e2-3', source: '2', target: '3', animated: true }
-  ];
 
+  const [nodes, setNodes]: UseState<FlowNode[]> = useState<FlowNode[]>(sampleNodes);
+  const [edges, setEdges]: UseState<FlowEdge[]> = useState<FlowEdge[]>([]);
+  const [XYflowInstance, setXYflowInstance]: UseState<ReactFlowInstance<FlowNode, FlowEdge> | null> =
+    useState<ReactFlowInstance<FlowNode, FlowEdge> | null>(null);
   const flowWrapperRef: UseRef<HTMLDivElement> = useRef(null);
-  const [nodes, setNodes]: UseState<FlowNode[]> = useState<FlowNode[]>(initialNodes);
-  const [edges, setEdges]: UseState<FlowEdge[]> = useState<FlowEdge[]>(initialEdges);
-  // const { deleteElements } = useReactFlow();
 
-  // const getId = (): string => `${FLOW_NODE_PREFIX}-${new Date().getTime()}`;
+  const onInit = (instance: ReactFlowInstance<FlowNode, FlowEdge>): void => {
+    setXYflowInstance(instance);
+  };
 
   const onNodesChange = useCallback(
     (changes: NodeChange<FlowNode>[]): void =>
       setNodes((nds: FlowNode[]): FlowNode[] => applyNodeChanges(changes, nds)),
     [setNodes]
   );
-
   const onEdgesChange = useCallback(
     (changes: EdgeChange<FlowEdge>[]): void =>
       setEdges((eds: FlowEdge[]): FlowEdge[] => applyEdgeChanges(changes, eds)),
@@ -70,6 +58,36 @@ export default function XYFlow(): React.JSX.Element {
     (connection: Connection): void => setEdges((eds: FlowEdge[]): FlowEdge[] => addEdge(connection, eds)),
     [setEdges]
   );
+
+  const onDrop = useCallback(
+    (event: DragEvent): void => {
+      event.preventDefault();
+
+      const XYFlowBounds: DOMRect | undefined = flowWrapperRef.current?.getBoundingClientRect();
+      if (!XYFlowBounds || !XYflowInstance) return;
+
+      const type: FLOW_OBJECT_COLOR_KEY = event.dataTransfer.getData(DATA_TRANSFER_TYPE) as FLOW_OBJECT_COLOR_KEY;
+      const position: XYPosition | undefined = XYflowInstance?.screenToFlowPosition({
+        x: event.clientX - XYFlowBounds.left,
+        y: event.clientY - XYFlowBounds.top
+      });
+
+      const newNode: FlowNode = {
+        id: getId(),
+        ...(type === 'input' || type === 'output' ? { type } : {}),
+        position,
+        data: { label: `${type} node` },
+        style: { backgroundColor: FLOW_OBJECT_COLORS[type] }
+      };
+      setNodes((nds: FlowNode[]): FlowNode[] => nds.concat(newNode));
+    },
+    [XYflowInstance]
+  );
+
+  const onDragOver = useCallback((event: DragEvent): void => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = 'move';
+  }, []);
 
   const nodeColor = (node: { type?: FLOW_OBJECT_COLOR_KEY | string }): FLOW_OBJECT_COLOR_VALUE => {
     switch (node.type) {
@@ -82,6 +100,9 @@ export default function XYFlow(): React.JSX.Element {
     }
   };
 
+  console.log(nodes);
+  console.log(edges);
+
   return (
     <ReactFlowProvider>
       <section ref={flowWrapperRef} className={styles.mainContent}>
@@ -89,16 +110,19 @@ export default function XYFlow(): React.JSX.Element {
           <Sidebar />
         </aside>
         <ReactFlow
+          onInit={onInit}
           nodes={nodes}
           edges={edges}
           onNodesChange={onNodesChange}
           onEdgesChange={onEdgesChange}
           onConnect={onConnect}
+          onDrop={onDrop}
+          onDragOver={onDragOver}
           fitView
         >
           <Background color="#dadada" variant={BackgroundVariant.Lines} />
           <MiniMap nodeColor={nodeColor} nodeStrokeWidth={3} zoomable pannable />
-          <Controls className={styles.controls}></Controls>
+          <Controls className={styles.controls} />
         </ReactFlow>
       </section>
     </ReactFlowProvider>
